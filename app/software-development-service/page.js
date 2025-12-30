d8cd23cd954a81183c181ffe9cea376abc45eb13
sathiyapriya-b <sathiyapriya.b@codework.ai>
2025-12-30 15:31:30 +0530

    resolved

--- app/software-development-service/page.js
+++ app/software-development-service/page.js
@@ -3,11 +3,7 @@ import SoftwareLanding from '../components/softwareDevelopment/softwareLanding'
 import SoftwareDevelopment from '../components/softwareDevelopment/softwareDevelopment'
 import AgileSoftwareProcess from '../components/softwareDevelopment/agileSoftwareProcess'
 import SoftwareSolutions from '../components/softwareDevelopment/softwareSolutions'
-export const metadata = {
-  title: 'Software Development Life Cycle (SDLC): Full Guide',
-  description: 'Explore SDLC phases, models and how DevOps and CI/CD improve software development from planning through testing to deployment and maintenance. ',
-  keywords: ['software development life cycle explained','Phases of SDLC with examples','SDLC models comparison waterfall agile ','role of DevOps in SDLC ','SDLC best practices for enterprises ','agile vs traditional SDLC models ','SDLC process for web applications ','importance of SDLC in software engineering ','SDLC stages from planning to maintenance ','modern software development life cycle approach '],
-}
+
 const page = () => {
   return (
     <div>
